a9a68df73871b2c9ae22dd560410c79a0a0554f7
Rokas <rokas@gamingmix.eu>
2020-10-31 20:15:32 +0200

    fixed bugs and removed vs files

--- UCOS/DiskRead.asm
+++ UCOS/DiskRead.asm
@@ -5,15 +5,13 @@ ReadDisk:
 
 	mov ah, 0x02
 	mov bx, PROGRAM_SPACE
-	mov al,4 ; if program doesnt work, possible solution is to increase the sector count
+	mov al,64 ; if program doesnt work, possible solution is to increase the sector count
 	mov dl, [BOOT_DISK]
 	mov ch, 0x00
 	mov dh, 0x00
 	mov cl, 0x02
 
 	int 0x13
-
-	jc DiskReadFailed
 
 	ret
 
--- UCOS/compile.bat
+++ UCOS/compile.bat
@@ -4,10 +4,19 @@ echo Compiling bootloader
 nasm bootloader.asm -f bin -o bootloader.bin
 
 echo Compiling Extension
-nasm ExtendedProgram.asm -f bin -o ExtendedProgram.bin
+nasm ExtendedProgram.asm -f elf64 -o ExtendedProgram.o
 
-echo Combining into executable floppy
-copy /b bootloader.bin+ExtendedProgram.bin bootloader.flp
+echo Compiling Kernel.cpp
+wsl $WSLENV/x86_64-elf-gcc -ffreestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "Kernel.o"
+
+echo Linking Extension with Kernel
+wsl $WSLENV/x86_64-elf-ld -o kernel.tmp -Ttext 0x7e00 ExtendedProgram.o Kernel.o
+
+echo Converting Linked objects to Binaries
+wsl $WSLENV/x86_64-elf-objcopy -O binary kernel.tmp kernel.bin
+
+echo Combining into executable binary
+copy /b bootloader.bin+kernel.bin bootloader.flp
 
 echo Done
 
