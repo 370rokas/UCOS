37b8d965c44f5259fef9a0b3c41eeecd4c1a6abc
Rokas <rokas@gamingmix.eu>
2020-10-31 15:25:53 +0200

    Moved print to another file

--- UCOS/bootloader.asm
+++ UCOS/bootloader.asm
@@ -9,20 +9,7 @@ call PrintString
 
 jmp $
 
-PrintString:
-	mov ah, 0x0e
-	.Loop:
-	cmp [bx], byte 0
-	je .Exit
-		mov al, [bx]
-		int 0x10
-		inc bx
-		jmp .Loop
-	.Exit:
-	ret
-
-StringToOutput:
-	db 'Hello there!',0
+%include "print.asm"
 
 times 510-($-$$) db 0
 
