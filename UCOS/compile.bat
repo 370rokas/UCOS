9767c2ac786e8b286e632c618fe9fcdcf78ffeb4
Rokas <rokas@gamingmix.eu>
2020-10-31 15:44:14 +0200

    Updated compilation file

--- UCOS/compile.bat
+++ UCOS/compile.bat
@@ -1,2 +1,14 @@
-nasm bootloader.asm -f bin -o bootloader.flp
+@echo off
+
+echo Compiling bootloader
+nasm bootloader.asm -f bin -o bootloader.bin
+
+echo Compiling Extension
+nasm ExtendedProgram.asm -f bin -o ExtendedProgram.bin
+
+echo Combining into executable floppy
+copy /b bootloader.bin+ExtendedProgram.bin bootloader.flp
+
+echo Done
+
 pause
